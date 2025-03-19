86ce654c30b09cd5b73a791757f1e43472ae917f
prasanna7codes <prasannalit098@gmail.com>
2025-03-20 02:12:03 +0530

    working with the Icons from react-native

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+
+
+
+function App(): React.JSX.Element {
+  
+
+
+
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default App;
---
+++ src/components/Icons.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import type { PropsWithChildren } from 'react'
+import Icon from '@react-native-vector-icons/fontawesome6'
+type IconsProps=PropsWithChildren<{
+    name:string;
+}>
+
+const Icons = ({name}:IconsProps)=> {
+  switch (name) {
+    case 'circle':
+        return < Icon name="circle" size={38}/>
+        
+    
+
+    case 'cross':
+            return < Icon name="cross" size={38} iconStyle="solid"/>
+            
+
+    default:
+        return < Icon name="pencil" size={38} iconStyle="solid"/>
+  }
+}
+
+export default Icons
