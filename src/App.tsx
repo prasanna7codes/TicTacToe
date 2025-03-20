e98657ced4c721eb92182ac0df413d4fcba7bcec
prasanna7codes <prasannalit098@gmail.com>
2025-03-20 19:56:52 +0530

    adding styles and conditiinal rendering the top part

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@
 import React, { useState } from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
@@ -35,7 +36,7 @@ function App(): React.JSX.Element {
   const [gameWinner,setGameWinner]=useState('')
   const [gameState, setGameState] = useState(new Array(9).fill('empty'))
 
-    const reloadGame =() => {
+  const reloadGame =() => {
       setIsCross(false);
       setGameWinner('');
       setGameState(new Array(9).fill('empty'))
@@ -43,7 +44,7 @@ function App(): React.JSX.Element {
     }
 
 
-    const checkIsWinner = () => {
+  const checkIsWinner = () => {
 
     if (
       gameState[0] === gameState[1] &&
@@ -99,25 +100,25 @@ function App(): React.JSX.Element {
     }
 
 
-    const OnChangeItem = (itemNumber:number) => {
+  const OnChangeItem = (itemNumber:number) => {
 
       if (gameWinner) {
         return Snackbar.show({
           text: gameWinner,
           backgroundColor: '#000000',
-          textColor: "#FFFFFF"
+          textColor: '#FFFFFF'
         })
       }
 
       if (gameState[itemNumber] === 'empty') {
-        gameState[itemNumber] = isCross ? 'cross': 'circle'
+        gameState[itemNumber] = isCross ? 'cross' : 'circle'
         setIsCross(!isCross)
       }
       else {
         return Snackbar.show({
-          text: "Position is already filled",
-          backgroundColor: "red",
-          textColor: "#FFF"
+          text: 'Position is already filled',
+          backgroundColor: 'red',
+          textColor: '#FFF'
         })
       } 
           
@@ -130,14 +131,128 @@ function App(): React.JSX.Element {
 
 
   return (
-    <View>
-      <Text>Hello</Text>
-    </View>
+   <SafeAreaView>
+
+{gameWinner ? (
+        <View style={[styles.playerInfo, styles.winnerInfo]}>
+          <Text style={styles.winnerTxt}>{gameWinner}</Text>
+        </View>
+      ) : (
+        <View
+        style={[
+          styles.playerInfo,
+          isCross ? styles.playerX : styles.playerO
+        ]}
+        >
+          <Text style={styles.gameTurnTxt}>
+            Player {isCross? 'X' : 'O'}'s Turn
+          </Text>
+        </View>
+      )}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
- 
+  playerInfo: {
+    height: 56,
+
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    borderRadius: 4,
+    paddingVertical: 8,
+    marginVertical: 12,
+    marginHorizontal: 14,
+
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowColor: '#333',
+    shadowOpacity: 0.2,
+    shadowRadius: 1.5,
+  },
+  gameTurnTxt: {
+    fontSize: 20,
+    color: '#FFFFFF',
+    fontWeight: '600',
+  },
+  playerX: {
+    backgroundColor: '#38CC77',
+  },
+  playerO: {
+    backgroundColor: '#F7CD2E',
+  },
+  grid: {
+    margin: 12,
+  },
+  card: {
+    height: 100,
+    width: '33.33%',
+
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    borderWidth: 1,
+    borderColor: '#333',
+  },
+  winnerInfo: {
+    borderRadius: 8,
+    backgroundColor: '#38CC77',
+
+    shadowOpacity: 0.1,
+  },
+  winnerTxt: {
+    fontSize: 20,
+    color: '#FFFFFF',
+    fontWeight: '600',
+    textTransform: 'capitalize',
+  },
+  gameBtn: {
+    alignItems: 'center',
+
+    padding: 10,
+    borderRadius: 8,
+    marginHorizontal: 36,
+    backgroundColor: '#8D3DAF',
+  },
+  gameBtnText: {
+    fontSize: 18,
+    color: '#FFFFFF',
+    fontWeight: '500',
+  },
 });
 
 export default App;
