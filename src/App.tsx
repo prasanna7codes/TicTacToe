fbb28f75287bbc5674b13b4c7d1061b56db71582
prasanna7codes <prasannalit098@gmail.com>
2025-03-20 19:47:32 +0530

    writing logic for reloading the game, checking the winner and changing the gamestate array

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   ScrollView,
@@ -15,6 +15,8 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
+
+import Snackbar from 'react-native-snackbar';
 
 import {
   Colors,
@@ -29,7 +31,101 @@ import {
 
 
 function App(): React.JSX.Element {
-  
+  const [isCross,setIsCross]=useState(false)
+  const [gameWinner,setGameWinner]=useState('')
+  const [gameState, setGameState] = useState(new Array(9).fill('empty'))
+
+    const reloadGame =() => {
+      setIsCross(false);
+      setGameWinner('');
+      setGameState(new Array(9).fill('empty'))
+
+    }
+
+
+    const checkIsWinner = () => {
+
+    if (
+      gameState[0] === gameState[1] &&
+      gameState[0] === gameState[2] &&
+      gameState[0] !== 'empty'
+    ) {
+      setGameWinner(`${gameState[0]} won the game! 🥳`);
+    } else if (
+      gameState[3] !== 'empty' &&
+      gameState[3] === gameState[4] &&
+      gameState[4] === gameState[5]
+    ) {
+      setGameWinner(`${gameState[3]} won the game! 🥳`);
+    } else if (
+      gameState[6] !== 'empty' &&
+      gameState[6] === gameState[7] &&
+      gameState[7] === gameState[8]
+    ) {
+      setGameWinner(`${gameState[6]} won the game! 🥳`);
+    } else if (
+      gameState[0] !== 'empty' &&
+      gameState[0] === gameState[3] &&
+      gameState[3] === gameState[6]
+    ) {
+      setGameWinner(`${gameState[0]} won the game! 🥳`);
+    } else if (
+      gameState[1] !== 'empty' &&
+      gameState[1] === gameState[4] &&
+      gameState[4] === gameState[7]
+    ) {
+      setGameWinner(`${gameState[1]} won the game! 🥳`);
+    } else if (
+      gameState[2] !== 'empty' &&
+      gameState[2] === gameState[5] &&
+      gameState[5] === gameState[8]
+    ) {
+      setGameWinner(`${gameState[2]} won the game! 🥳`);
+    } else if (
+      gameState[0] !== 'empty' &&
+      gameState[0] === gameState[4] &&
+      gameState[4] === gameState[8]
+    ) {
+      setGameWinner(`${gameState[0]} won the game! 🥳`);
+    } else if (
+      gameState[2] !== 'empty' &&
+      gameState[2] === gameState[4] &&
+      gameState[4] === gameState[6]
+    ) {
+      setGameWinner(`${gameState[2]} won the game! 🥳`);
+    } else if (!gameState.includes('empty', 0)) {
+      setGameWinner('Draw game... ⌛️');
+    }
+    }
+
+
+    const OnChangeItem = (itemNumber:number) => {
+
+      if (gameWinner) {
+        return Snackbar.show({
+          text: gameWinner,
+          backgroundColor: '#000000',
+          textColor: "#FFFFFF"
+        })
+      }
+
+      if (gameState[itemNumber] === 'empty') {
+        gameState[itemNumber] = isCross ? 'cross': 'circle'
+        setIsCross(!isCross)
+      }
+      else {
+        return Snackbar.show({
+          text: "Position is already filled",
+          backgroundColor: "red",
+          textColor: "#FFF"
+        })
+      } 
+          
+            checkIsWinner();
+
+    }
+
+
 
 
 
